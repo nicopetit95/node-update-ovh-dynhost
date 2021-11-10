e385294d79b6007dd06b8514e53ad93f4ae234a5
Nicolas PETIT <npetit@gtp-conseil.fr>
2021-11-10 14:11:35 +0100

    Connection to the OVH API

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+require('dotenv-flow').config()
 const ip = require('ip')
-console.log("ip", ip.address())
+const ovh = require('ovh')
+
+console.log('OVH_APP_KEY', process.env.OVH_APP_KEY)
+console.log('OVH_SECRET_KEY', process.env.OVH_SECRET_KEY)
+console.log('OVH_CONSUMER_KEY', process.env.OVH_CONSUMER_KEY)
+
+const ipAddress = ip.address()
+const ovhInstance = ovh({
+  endpoint: 'ovh-eu',
+  appKey: process.env.OVH_APP_KEY,
+  appSecret: process.env.OVH_SECRET_KEY,
+  consumerKey: process.env.OVH_CONSUMER_KEY
+})
+
+console.log(ipAddress, ovhInstance)
